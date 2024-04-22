c8e552014607030b5e6837d74e59f1f2d1991027
rudiko <chakryan_rudik@mail.ru>
2024-04-22 16:55:13 +0300

    add router

---
+++ src/pages/main/main.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from '../../styles/Main.module.css'
+
+const Main = () => {
+    return (
+        <div>
+            Main
+        </div>
+    );
+};
+
+export default Main;
